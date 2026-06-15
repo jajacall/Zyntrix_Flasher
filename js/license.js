a1bbae06f3d0cc739f1c15bfd1ec4bca051745fb
Ale <147809157+jajacall@users.noreply.github.com>
2026-06-15 16:25:14 +0800

    Update license.js

--- js/license.js
+++ js/license.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL =
-    "https://pukkpjluyjrdqwusskfl.supabase.co/rest/v1/";
+    "https://pukkpjluyjrdqwusskfl.supabase.co";
 
 const SUPABASE_KEY =
     "sb_publishable_W5-_1mnFcHtY9-FOk4IA3w_MGREKs9u";
