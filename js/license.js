27a78d3e5b1af7653e09f66ea0cbdb8e1216381c
Ale <147809157+jajacall@users.noreply.github.com>
2026-06-15 21:40:47 +0800

    Update license.js

--- js/license.js
+++ js/license.js
@@ -1,15 +1,3 @@
-const SUPABASE_URL = 
-    "https://pukkpjluyjrdqwusskfl.supabase.co";
-
-const SUPABASE_KEY = 
-    "sb_publishable_W5-_1mnFcHtY9-FOk4IA3w_MGREKs9u";
-
-const sb =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    );
-
 let verifiedLicense = null;
 
 async function verifyLicense() {
