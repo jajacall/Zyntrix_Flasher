cff969fa68f66b59bd3c9a4999952eaa5b19eed2
Ale <147809157+jajacall@users.noreply.github.com>
2026-06-15 20:59:21 +0800

    Update github-releases.js

--- js/github-releases.js
+++ js/github-releases.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL =
+const SUPABASE_URL = 
     "https://pukkpjluyjrdqwusskfl.supabase.co";
 
-const SUPABASE_KEY =
+const SUPABASE_KEY = 
     "sb_publishable_W5-_1mnFcHtY9-FOk4IA3w_MGREKs9u";
 
 const sb =
