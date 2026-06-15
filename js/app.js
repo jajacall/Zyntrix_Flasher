9a56315a9a3b59baabded7d76af433ff3bc2e80b
Ale <147809157+jajacall@users.noreply.github.com>
2026-06-15 13:10:15 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,3 +11,54 @@ document.addEventListener("DOMContentLoaded", () => {
         .addEventListener("change", updateManifest);
 
 });
+
+async function updateManifest() {
+
+    const version =
+        document.getElementById("firmwareSelect").value;
+
+    const assets = firmwareData[version];
+
+    const appBin =
+        assets.find(a => a.name.endsWith(".ino.bin"));
+
+    const bootloader =
+        assets.find(a => a.name.includes("bootloader"));
+
+    const partitions =
+        assets.find(a => a.name.includes("partitions"));
+
+    const manifest = {
+        name: `ZYNTRIX ${version}`,
+        version: version,
+        builds: [{
+            chipFamily: "ESP32",
+            parts: [
+                {
+                    path: bootloader.browser_download_url,
+                    offset: 4096
+                },
+                {
+                    path: partitions.browser_download_url,
+                    offset: 32768
+                },
+                {
+                    path: appBin.browser_download_url,
+                    offset: 65536
+                }
+            ]
+        }]
+    };
+
+    const manifestUrl =
+        URL.createObjectURL(
+            new Blob(
+                [JSON.stringify(manifest)],
+                { type: "application/json" }
+            )
+        );
+
+    document
+        .querySelector("esp-web-install-button")
+        .setAttribute("manifest", manifestUrl);
+}
