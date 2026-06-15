11a207abad7b85b42ed68e0d0116943291673d80
Ale <147809157+jajacall@users.noreply.github.com>
2026-06-15 21:40:14 +0800

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+    "https://pukkpjluyjrdqwusskfl.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_W5-_1mnFcHtY9-FOk4IA3w_MGREKs9u";
+
+const sb =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
